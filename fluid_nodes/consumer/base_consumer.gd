9fab59b096fdb5c17fa3171a8d4a1cae4ad3a783
9thAzure <dl6741@yahoo.ca>
2024-07-02 21:26:25 -0700

    consumers deal with inefficiencies by reducing flow friction of input pipes, where ever possible

--- fluid_nodes/consumer/base_consumer.gd
+++ fluid_nodes/consumer/base_consumer.gd
@@ -20,6 +20,7 @@ func _update() -> void:
 	if inflowing_rate < consumption_rate:
 		efficiency = inflowing_rate / consumption_rate
 		extra_flow_rate = 0.0
+		_request_more_flow()
 		return
 	
 	efficiency = 1.0
@@ -29,3 +30,34 @@ func _update() -> void:
 		return
 
 	_handle_backflow()
+
+func _request_more_flow() -> void:
+	var required_flow_increase := consumption_rate * (1 - efficiency)
+	var total_flow_friction := 0.0
+	for connection in connections:
+		total_flow_friction += connection.flow_friction
+	
+	print(required_flow_increase, total_flow_friction)
+	
+	if total_flow_friction <= required_flow_increase:
+		if is_zero_approx(total_flow_friction):
+			return
+
+		for connection in connections:
+			if is_zero_approx(connection.flow_friction):
+				continue
+
+			connection.flow_friction = 0
+			connection.get_connecting_node(self).queue_update()
+		return
+
+	var decrease_rate := 1 - required_flow_increase / total_flow_friction
+	for connection in connections:
+		if is_zero_approx(connection.flow_friction):
+			continue
+
+		connection.flow_friction *= decrease_rate
+		if is_zero_approx(connection.flow_friction):
+			connection.flow_friction = 0
+		
+		connection.get_connecting_node(self).queue_update()
