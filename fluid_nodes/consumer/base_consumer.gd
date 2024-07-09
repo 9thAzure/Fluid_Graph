10d2bb2e198d1d7adef2db001a53c0b512707581
9thAzure <dl6741@yahoo.ca>
2024-07-08 22:39:37 -0700

    fix consumption_rate  error on startup, removed override of _request_more_flow in consumer

--- fluid_nodes/consumer/base_consumer.gd
+++ fluid_nodes/consumer/base_consumer.gd
@@ -6,7 +6,8 @@ class_name BaseConsumer
 var consumption_rate := 0.0:
 	set(value):
 		consumption_rate = value
-		queue_update()
+		if not Engine.is_editor_hint() and is_inside_tree():
+			queue_update()
 
 var efficiency := 1.0
 
@@ -37,36 +38,3 @@ func _update() -> void:
 		return
 
 	_handle_backflow()
-
-func _request_more_flow() -> void:
-	# TODO: implement
-	pass
-	# var required_flow_increase := consumption_rate * (1 - efficiency)
-	# var total_flow_friction := 0.0
-	# for connection in connections:
-	# 	total_flow_friction += connection.flow_friction
-	
-	# print(required_flow_increase, total_flow_friction)
-	
-	# if total_flow_friction <= required_flow_increase:
-	# 	if is_zero_approx(total_flow_friction):
-	# 		return
-
-	# 	for connection in connections:
-	# 		if is_zero_approx(connection.flow_friction):
-	# 			continue
-
-	# 		connection.flow_friction = 0
-	# 		connection.get_connecting_node(self).queue_update()
-	# 	return
-
-	# var decrease_rate := 1 - required_flow_increase / total_flow_friction
-	# for connection in connections:
-	# 	if is_zero_approx(connection.flow_friction):
-	# 		continue
-
-	# 	connection.flow_friction *= decrease_rate
-	# 	if is_zero_approx(connection.flow_friction):
-	# 		connection.flow_friction = 0
-		
-	# 	connection.get_connecting_node(self).queue_update()
